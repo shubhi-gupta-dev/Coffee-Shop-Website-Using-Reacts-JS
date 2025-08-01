da61d42de404e159dde1d50068ed2c2bc97ae0a5
shubhi-gupta-dev <shubhi.gupta3579@gmail.com>
2025-08-01 11:18:41 +0530

    banner com updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,21 +4,28 @@ const Banner = () => {
   return (
     <div
       id="home"
-      className="relative bg-cover bg-no-repeat w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-screen"
+      className="relative bg-cover bg-no-repeat w-full h-[400px] sm:h-[400px] md:h-[400px] lg:h-screen "
     >
-      <div className="absolute inset-0 opacity-50 flex flex-col items-center">
-        <div className="relative z-10 max-w-xl text-white text-center sm:text-center md:text-center lg:text-left pt-10">
-          <h1 className="px-5 mb-4 text-3xl font-bold font-playFairDisplay sm:text-5xl md:text-7xl lg:text-7xl">
+      <div className="absolute inset-0 flex flex-col">
+        <div className="relative z-10 max-w-xl text-white text-center sm:text-center md:text-center lg:text-left pt-10 md:left-24 ">
+          <h1 className="px-5 mb-4 text-6xl font-bold font-playFairDisplay sm:text-5xl md:text-7xl lg:text-8xl">
             Coffee
           </h1>
 
-          <p className="px-5 leading-relaxed mb-6 font-serif sm:text-base">
-            It is best to start your day with a cup of coffee.Discover the best
+          <p className="px-5 leading-relaxed mb-6 font-serif sm:text-base font-light md:text-xl lg:text-[15px]">
+            It is best to start your day with a cup of coffee. Discover the best
             flavours of coffee you will ever have.we provide the best for our
             customers.
           </p>
+
+          <div className="px-5 flex justify-center lg:justify-start">
+             <button className="font-medium bg-yellow-600 hover:bg-yellow-700 text-black transition-colors rounded-full shadow-lg border-2 border-yellow-600 hover:border-yellow-700 px-6 py-3 sm:px-4 sm:py-2 md:py-3 md:px-6 lg:px-8 lg:py-4 text-lg">
+              Order Now
+             </button>
+          </div>
         </div>
       </div>
+
       <video
         className="w-full h-[400px] md:min-h-[300px] object-cover"
         autoPlay
@@ -26,7 +33,7 @@ const Banner = () => {
         muted
       >
         <source
-          src="https://videos.pexels.com/video-files/2909914/2909914-uhd_2732_1440_24fps.mp4"
+          src="https://videos.pexels.com/video-files/6201669/6201669-uhd_2560_1440_24fps.mp4"
           type="video/mp4"
         />
       </video>
